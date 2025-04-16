715a64af6b77bae24b7c25aaae4f72974252929a
Ibrohim Kalandarov <iqalandarov05@gmail.com>
2025-04-16 11:48:33 +0500

    first

--- src/main/java/com/example/pdponline/PdpOnlineApplication.java
+++ src/main/java/com/example/pdponline/PdpOnlineApplication.java
@@ -3,15 +3,12 @@ package com.example.pdponline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 @EnableCaching
 public class PdpOnlineApplication {
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(PdpOnlineApplication.class, args);
     }
 
