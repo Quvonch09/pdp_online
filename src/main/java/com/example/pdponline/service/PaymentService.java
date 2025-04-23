07c5a7d34e35ea0add4abc05685244511ebc5faf
codot-09 <onabiyev626@gmail.com>
2025-04-23 14:30:34 +0500

    payment xatolik

--- src/main/java/com/example/pdponline/service/PaymentService.java
+++ src/main/java/com/example/pdponline/service/PaymentService.java
@@ -18,6 +18,8 @@ import com.example.pdponline.repository.PaymentRepository;
 import com.example.pdponline.repository.PromoCodeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
@@ -120,6 +122,7 @@ public class PaymentService {
      * @param moduleIds modullar bo'yicha
      * @return payment lar listi
      */
+    @Transactional
     public ApiResponse<?> getPayments(
             LocalDate startDate,
             LocalDate endDate,
@@ -131,9 +134,12 @@ public class PaymentService {
             Double endAmount,
             List<Long> moduleIds
     ) {
-        List<Payment> payments = paymentRepository.findPayments(
-                startDate, endDate, type, status, studentId, promoCode, startAmount, endAmount, moduleIds
+        Specification<Payment> spec = PaymentSpecification.filter(
+                startDate, endDate, type, status,
+                studentId, promoCode, startAmount, endAmount, moduleIds
         );
+
+        List<Payment> payments = paymentRepository.findAll((Sort) spec);
 
         if (payments.isEmpty()) {
             log.warn("Paymentlar mavjud emas");
@@ -147,7 +153,7 @@ public class PaymentService {
             return PaymentMapper.toDto(moduleDtos, payment);
         }).toList();
 
-        log.info("Paymentlar topildi: {}",paymentDTOS.size() );
+        log.info("Paymentlar topildi: {}", paymentDTOS.size());
         return ApiResponse.successResponse(paymentDTOS);
     }
 }
